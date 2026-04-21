cb9d91b6cfc7fffaeaf98d4a725a3f907b292a8d
rzv12-09 <olariurazvan20@gmail.com>
2026-04-21 22:14:23 +0300

    feat: implement React UI for adding and deleting ingredients

--- frontend/src/services/ingredientsService.js
+++ frontend/src/services/ingredientsService.js
@@ -1,8 +1,37 @@
+const baseUrl = import.meta.env.VITE_API_URL;
+
 export const getIngredients = async () => {
-  const baseUrl = import.meta.env.VITE_API_URL;
   const response = await fetch(`${baseUrl}/api/ingredients`);
   if (!response.ok) {
     throw new Error('Failed to fetch inredients from server');
   }
+  return await response.json();
+};
+
+export const addIngredient = async (ingredientData) => {
+  const response = await fetch(`${baseUrl}/api/ingredients`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(ingredientData),
+  });
+  if (!response.ok) throw new Error('Failed to add ingredient');
+  return await response.json();
+};
+
+export const updateIngredient = async (id, ingredientData) => {
+  const response = await fetch(`${baseUrl}/api/ingredients/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(ingredientData),
+  });
+  if (!response.ok) throw new Error('Failed to update ingredient');
+  return await response.json();
+};
+
+export const deleteIngredient = async (id) => {
+  const response = await fetch(`${baseUrl}/api/ingredients/${id}`, {
+    method: 'DELETE',
+  });
+  if (!response.ok) throw new Error('Failed to delete ingredient');
   return await response.json();
 };
